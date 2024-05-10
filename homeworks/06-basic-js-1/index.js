8dcc1cb1bfc3c3370bfc957277625407827e7411
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2024-05-10 18:01:51 +0200

    [Homework #6] - JS-basic-1 by Vladyslav Pohorilyi

--- homeworks/06-basic-js-1/index.js
+++ homeworks/06-basic-js-1/index.js
@@ -1,6 +1,6 @@
 // task 1  opposite http://www.codewars.com/kata/opposite-number
 
-const opposite = number => number ? -number: number
+const opposite = number => number>0 ? -number: number
 
 //end task 1
 
@@ -35,16 +35,18 @@ function printArray(array){
 function rentalCarCost(d) {
     const costRent = 40;
 
-    let resultAmount;
-
-    if(d<3){
-        resultAmount=  costRent*d;
-    }else if (d<=6){
-        resultAmount = costRent*d-20;
-    }else if(d>=7){
-        resultAmount=  costRent*d-50
+    switch(d>0){
+        case (d<3):
+            return costRent*d;
+        case (d<=6):
+            return costRent*d-20;
+        case (d>=7):
+            return costRent*d-50;
+        default:
+            return 'error';
     }
-    return resultAmount;
+
+
 }
 
 // end task 4
@@ -83,6 +85,16 @@ function getMiddle(s)
 // task 10 https://www.codewars.com/kata/find-the-parity-outlier
 
 // task 11 https://www.codewars.com/kata/zipwith
+function zipWith(fn,a0,a1) {
+    let result =[]
+    let length = Math.min(a0.length,a1.length)
+    for(let i =0; i<length;i++){
+        result.push(fn(a0[i],a1[i]) )
+    }
+    return result
+}
+// end task 11
+
 // task 12    https://www.codewars.com/kata/filter-the-number
 
 var filterString = function(value) {
@@ -95,11 +107,71 @@ var filterString = function(value) {
 // end task 12
 
 // task 13 https://www.codewars.com/kata/n-th-fibonacci
+function nthFibo(n) {
+    if (n <= 0) {
+        return "Invalid input.";
+    } else if (n === 1) {
+        return 0;
+    } else if (n === 2) {
+        return 1;
+    } else {
+        return nthFibo(n - 1) + nthFibo(n - 2);
+    }
+}
+
+// end task 13
+
 // task 14 https://www.codewars.com/kata/cat-and-mouse-2d-version/
 // task 15 https://www.codewars.com/kata/duplicate-encoder
 // task 16 https://www.codewars.com/kata/5693239fb761dc8670000001
 // task 17  https://www.codewars.com/kata/576757b1df89ecf5bd00073b
+function towerBuilder(nFloors) {
+    // build here
+    let result =[];
+    let widthRow = nFloors*2-1;
+
+    for(let i =0; i<nFloors ;i++){
+        let calculate = i*2+1;
+        let star = '*'.repeat(calculate);
+        let space = ' '.repeat((widthRow - calculate )/2)
+        result.push(space+star+space)
+    }
+    return result
+}
+
 // task 18 https://www.codewars.com/kata/58f5c63f1e26ecda7e000029
+function wave(str){
+    let result =[];
+
+    for(let i=0; i< str.length;i++){
+        if(str[i] !== ' '){
+            let letter = str[i].toUpperCase();
+            result.push(str.slice(0,i)+letter+str.slice(i+1,str.length))
+        }
+    }
+    return result
+
+}
+// end task 18
+
 // task 19  https://www.codewars.com/kata/59d398bb86a6fdf100000031
+function stringBreakers(n, string){
+    let stringWithoutSpace = string.replace(/\s/g, '')
+
+    let result =[]
+    for(let i = 0; i<string.length;i+=n){
+
+        result.push(stringWithoutSpace.slice(i,i+n))
+    }
+    return result.join('\n')
+}
+// end task 19
 // task 20 https://www.codewars.com/kata/514a024011ea4fb54200004b
+function domainName(url){
+    //your code here
+    let r = url.replace('https://','').replace('http://','').replace('www.','')
+
+    return r.split('.')[0]
+}
+// end task 20
 
