3d28b5e4f58c0aad56103ce3b1c3beb49ee3f23b
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2024-05-29 17:14:42 +0200

    [Homework #11] - JS-dom by Vladyslav Pohorilyi

---
+++ homeworks/11-dom/main.js
@@ -0,0 +1,26 @@
+const gridTable = document.querySelector('#grid-table');
+
+
+for(let row =1; row<=30; row++ ){
+    for(let col =1; col<=20; col++ ){
+        const createDiv = document.createElement('div');
+        createDiv.classList.add('cell');
+        createDiv.dataset.row = row;
+        createDiv.dataset.col = col;
+        gridTable.append(createDiv)
+    }
+}
+
+gridTable.addEventListener('click', (event) => {
+
+    const {row,col} =event.target.dataset
+    event.target.classList.add('selected')
+    event.target.innerText=`${col}/${row}`
+
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.classList.remove( 'active-row', 'active-column');
+    });
+
+    document.querySelectorAll(`.cell[data-row="${row}"]`).forEach(cell => cell.classList.add('active-row'));
+    document.querySelectorAll(`.cell[data-col="${col}"]`).forEach(cell => cell.classList.add('active-column'));
+})
